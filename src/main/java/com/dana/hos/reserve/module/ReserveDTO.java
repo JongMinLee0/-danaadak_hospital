1fda119850f396345105168dfbb708f4e19b9b21
user1 <user1@DESKTOP-RQHRM91>
2019-09-06 17:45:07 +0900

    add : reserve

---
+++ src/main/java/com/dana/hos/reserve/module/ReserveDTO.java
@@ -0,0 +1,93 @@
+package com.dana.hos.reserve.module;
+
+public class ReserveDTO {
+	
+	private String re_date;
+	private String re_time;
+	private String category;
+	private String message;
+	private int re_state;
+	private String id;
+	private String hos_id;
+	private String medicine;
+	private String birth;
+	
+	public ReserveDTO() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public String getRe_date() {
+		return re_date;
+	}
+
+	public void setRe_date(String re_date) {
+		this.re_date = re_date;
+	}
+
+	public String getRe_time() {
+		return re_time;
+	}
+
+	public void setRe_time(String re_time) {
+		this.re_time = re_time;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	public String getMessage() {
+		return message;
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+
+	public int getRe_state() {
+		return re_state;
+	}
+
+	public void setRe_state(int re_state) {
+		this.re_state = re_state;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getHos_id() {
+		return hos_id;
+	}
+
+	public void setHos_id(String hos_id) {
+		this.hos_id = hos_id;
+	}
+
+	public String getMedicine() {
+		return medicine;
+	}
+
+	public void setMedicine(String medicine) {
+		this.medicine = medicine;
+	}
+
+	public String getBirth() {
+		return birth;
+	}
+
+	public void setBirth(String birth) {
+		this.birth = birth;
+	}
+	
+	
+	
+}//end class 
---
+++ src/main/java/com/dana/hos/reserve/repo/impl/ReserveDaoImp.java
@@ -0,0 +1,36 @@
+package com.dana.hos.reserve.repo.impl;
+
+import java.util.List;
+
+import org.apache.ibatis.session.SqlSession;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+
+import com.dana.hos.reserve.module.ReserveDTO;
+import com.dana.hos.reserve.repo.ReserveDAO;
+
+@Repository
+public class ReserveDaoImp implements ReserveDAO {
+
+	@Autowired //빈 주입 !(의존성 주입)
+	private SqlSession sqlSession;
+
+	public ReserveDaoImp() {
+	}
+	
+	public void setSqlSession(SqlSession sqlSession) {
+		this.sqlSession = sqlSession;
+	}
+
+	@Override
+	public List<ReserveDTO> reserveListMethod() {
+		return null;
+	}
+
+	@Override
+	public void re_registerMedthod(ReserveDTO dto) {
+		sqlSession.insert("re.ins",dto);
+	}
+
+	
+}// end class

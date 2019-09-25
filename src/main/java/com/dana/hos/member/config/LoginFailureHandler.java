7823c64418677ac9d456cca9635916d4ac92e5ab
JongMinLee0 <jongminlee0218@gmail.com>
2019-09-25 18:55:58 +0900

    개인, 병원 타입 맞게 설정..알람..

--- src/main/java/com/dana/hos/member/config/LoginFailureHandler.java
+++ src/main/java/com/dana/hos/member/config/LoginFailureHandler.java
@@ -6,6 +6,8 @@ import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.authentication.DisabledException;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.authentication.AuthenticationFailureHandler;
 import org.springframework.stereotype.Component;
@@ -14,18 +16,26 @@ import org.springframework.stereotype.Component;
 public class LoginFailureHandler implements AuthenticationFailureHandler {
 
 	@Override
-	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response,
-			AuthenticationException exception) throws IOException, ServletException {
-		
-		String username = request.getParameter("username");
-		
-		request.setAttribute("username", username);
-		
-		// 로그인 실패시 필요한 작업 추가
-		request.getRequestDispatcher("login?error=true").forward(request, response);
-	//	response.sendRedirect("login?error=true");
+	public void onAuthenticationFailure(HttpServletRequest request, HttpServletResponse response, AuthenticationException exception)
+            throws IOException, ServletException {
 
+		String username = request.getParameter("username");
+		String type = request.getParameter("type");
+		String errorMsg = "";
+		
+		if(exception instanceof DisabledException) {
+            errorMsg = "계정이 활성화되지 않았습니다.관리자의 승인 후 사용하실 수 있습니다.";
+        }else {
+        	errorMsg = "회원정보를 확인해주세요.";
+        }
+
+		request.setAttribute("username", username);
+		request.setAttribute("errorMsg", errorMsg);
+		request.setAttribute("type", type);
+
+		// 로그인 실패시 필요한 작업 추가
+		request.getRequestDispatcher("login").forward(request, response);
+		// response.sendRedirect("login?error=true");
 
 	}
 }
- 

8a2bacafe11e237cf9b25f31403ce2c3e44cc826
JongminLee <jongminlee0218@gmail.com>
2019-10-07 17:52:38 +0900

    add : 스마트 에디터 && 마이페이지 수정

--- src/main/java/com/dana/hos/comm/service/impl/SmartPhotoServiceImpl.java
+++ src/main/java/com/dana/hos/comm/service/impl/SmartPhotoServiceImpl.java
@@ -1,12 +1,7 @@
 package com.dana.hos.comm.service.impl;
 
-import java.io.File;
-import java.io.FileOutputStream;
 import java.io.InputStream;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.text.SimpleDateFormat;
-import java.util.UUID;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -17,9 +12,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.amazonaws.util.IOUtils;
 import com.dana.hos.comm.service.SmartPhotoService;
-
-import lombok.extern.slf4j.Slf4j;
 
 // 스마트 에디터 사진 업로드
 @Service
@@ -31,27 +25,30 @@ public class SmartPhotoServiceImpl implements SmartPhotoService {
 	S3Util s3 = new S3Util();
 	String bucketName = "danaatdak-bucket";
 
+	/*
+	 * InputStream is; byte[] bytes = IOUtils.toByteArray(is);
+	 */
+
 	@Override
 	public void smartUpload(HttpServletRequest request, HttpServletResponse response) {
 		try {
-			
+			InputStream is = request.getInputStream();
+			byte[] bytes = IOUtils.toByteArray(is);
 			///////////////////////
 			// 파일명을 받는다 - 일반 원본 파일명
 			String filename = request.getHeader("file-name");
 			System.out.println("file-name : " + filename);
 			// 업로드 경로
 			String uploadPath = "dak/images";
-			byte b[] = new byte[Integer.parseInt(request.getHeader("file-size"))];
-			
+
 			// S3에 업로드
 			ResponseEntity<String> img_path = new ResponseEntity<>(
-					UploadFileUtils.uploadFile(uploadPath, filename, b),
-					HttpStatus.CREATED);
-			
+					UploadFileUtils.uploadFile(uploadPath, filename, bytes), HttpStatus.CREATED);
+
 			// 이미지 경로
 			String image_path = img_path.getBody();
 			System.out.println("image_path : " + image_path);
-			String fullPath = "/comm/displayFile?fileName="+image_path;
+			String fullPath = "/hos/comm/displayFile?fileName=" + image_path;
 			System.out.println("fullPath : " + fullPath);
 
 			// 파일 정보
@@ -59,7 +56,7 @@ public class SmartPhotoServiceImpl implements SmartPhotoService {
 			// 정보 출력
 			sFileInfo += "&bNewLine=true";
 			// img 태그의 title 속성을 원본 파일명으로 적용시켜주기 위함
-			sFileInfo += "&sFileName=" + filename;
+			sFileInfo += "&sFileName=" + image_path;
 			sFileInfo += "&sFileURL=" + fullPath;
 			// System.out.println("sFileInfo : " + sFileInfo);
 			PrintWriter out = response.getWriter();
@@ -72,5 +69,4 @@ public class SmartPhotoServiceImpl implements SmartPhotoService {
 			logger.error(e.getMessage());
 		}
 	}
-
 }
